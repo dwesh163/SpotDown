6a7e32de2a429e38b2188b65762c05d162d3d433
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-11-16 21:34:26 +0100

    [add] findTrackId function

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ clientSecret = os.getenv("CLIENTSECRET")
 
 clientCredentialsManager = SpotifyClientCredentials(client_id=clientId, client_secret=clientSecret)
 sp = spotipy.Spotify(client_credentials_manager=clientCredentialsManager)
+
+
+def findTrackId(trackTitle):
+    results = sp.search(q=trackTitle, type='track', limit=1)
+
+    if results['tracks']['items']:
+        track_id = results['tracks']['items'][0]['id']
+        return track_id
+    else:
+        return None
+    
+
+trackTitle = input("Enter a track title : ")
+trackId = findTrackId(trackTitle)
+print(trackId)
